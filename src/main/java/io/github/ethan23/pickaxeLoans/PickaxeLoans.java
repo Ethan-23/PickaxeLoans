e4c0eb597ce9c27a3e8b973986c5d70150ebc503
Ethan-23 <emanroberts@gmail.com>
2026-07-15 14:11:05 -0400

    Initial Commit / Project Setup

---
+++ src/main/java/io/github/ethan23/pickaxeLoans/PickaxeLoans.java
@@ -0,0 +1,16 @@
+package io.github.ethan23.pickaxeLoans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PickaxeLoans extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
