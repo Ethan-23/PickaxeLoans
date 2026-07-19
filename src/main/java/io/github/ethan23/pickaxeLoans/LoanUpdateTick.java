2e649b9fc6dbe9bb3f326bc899a43445feee496f
Ethan-23 <emanroberts@gmail.com>
2026-07-19 09:00:49 -0400

    feat: added tick update checker
    - Created LoanUpdateTick.java
    - Added call to start LoanUpdateTick from PickaxeLoans.java

---
+++ src/main/java/io/github/ethan23/pickaxeLoans/LoanUpdateTick.java
@@ -0,0 +1,38 @@
+package io.github.ethan23.pickaxeLoans;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class LoanUpdateTick {
+
+    private static final int TICK_UPDATE_AMOUNT = 20;
+
+    private Integer taskID;
+    private final LoanService loanService;
+
+    public LoanUpdateTick(LoanService loanService) {
+        this.loanService = loanService;
+    }
+
+    public void startTickUpdate(Plugin plugin){
+        this.taskID = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
+            loanService.checkExpirationHeap();
+            handleEndsAtInventories(loanService.checkEndsAtHeap());
+        }, 0, TICK_UPDATE_AMOUNT);
+    }
+
+    public void cancelRepeatingTask(){
+        if(taskID != null){
+            Bukkit.getScheduler().cancelTask(taskID);
+        }
+    }
+
+    private void handleEndsAtInventories(Set<UUID> removedBorrowers){
+        for(UUID uuid : removedBorrowers){
+            PickaxeChecker.removeLoan(loanService, uuid);
+        }
+    }
+}
