e083b2052f6651426881a846cb6dbfdb9dba5df3
Ethan-23 <emanroberts@gmail.com>
2026-07-19 00:24:00 -0400

    fix: NamespacedKey swapped to lowercase

--- src/main/java/io/github/ethan23/pickaxeLoans/LoanKeys.java
+++ src/main/java/io/github/ethan23/pickaxeLoans/LoanKeys.java
@@ -3,5 +3,5 @@ package io.github.ethan23.pickaxeLoans;
 import org.bukkit.NamespacedKey;
 
 public class LoanKeys {
-    public static NamespacedKey loanKey = new NamespacedKey("pickaxeloans", "LOAN");
+    public static final NamespacedKey loanKey = new NamespacedKey("pickaxeloans", "loan");
 }
